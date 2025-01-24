ba0e37fdc9a56e5edc52c0e4252eb928eaf0afa6
tsu-na-gu <skybluecyclist@gmail.com>
2025-01-24 21:08:45 +0900

    add heroui

---
+++ src/app/components/Providers.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { ReactNode } from "react";
+import { HeroUIProvider } from "@heroui/react";
+
+export default function Providers({children}: {children: ReactNode}) {
+  return (
+    <HeroUIProvider>
+        {children}
+    </HeroUIProvider>
+  );
+}
